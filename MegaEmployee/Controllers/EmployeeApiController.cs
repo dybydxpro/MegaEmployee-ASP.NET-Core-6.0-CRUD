669c53333e38d31fd4d2ca2d32ec2a5e01f0dd51
venom0039 <65506774+venom0039@users.noreply.github.com>
2022-01-25 23:58:35 +0530

    Api Update 18:50 24-01-2020

---
+++ MegaEmployee/Controllers/EmployeeApiController.cs
@@ -0,0 +1,71 @@
+﻿using MegaEmployee.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaEmployee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeApiController : ControllerBase
+    {
+        private readonly AppDatabaseContext _db;
+
+        public EmployeeApiController(AppDatabaseContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AppDatabaseContext>>> Get()
+        {
+            return Ok(await _db.Employees.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<AppDatabaseContext>>> Get(int id)
+        {
+            var data = await _db.Employees.FindAsync(id);
+            if(data == null)
+            {
+                return BadRequest("Employee Data not found!");
+            }
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<AppDatabaseContext>>> Post(Employee emp)
+        {
+            _db.Employees.Add(emp);
+            await _db.SaveChangesAsync();
+            return Ok(await _db.Employees.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<AppDatabaseContext>>> Put(Employee emp)
+        {
+            var data = _db.Employees.Find(emp.Id);
+            if(data == null)
+            {
+                return BadRequest("Employee not found!");
+            }
+            data.Name = emp.Name;
+            data.Type = emp.Type;
+            await _db.SaveChangesAsync();
+            return Ok(await _db.Employees.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<AppDatabaseContext>>> Delete(int id)
+        {
+            var data = await _db.Employees.FindAsync(id);
+            if(data == null)
+            {
+                return BadRequest("Employee not found!");
+            }
+            _db.Employees.Remove(data);
+            await _db.SaveChangesAsync();
+            return Ok();    //await _db.Employees.ToListAsync()
+        }
+    }
+}
--- MegaEmployee/Program.cs
+++ MegaEmployee/Program.cs
@@ -6,13 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDatabaseContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" }));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
     app.UseHsts();
 }
 
